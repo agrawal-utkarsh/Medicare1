020d7edcbe4ab45191c9cc036e8db4136bfd8f1d
agrawal-utkarsh <utkarsh.agarwal@mobisy.com>
2022-03-20 13:13:43 +0530

    initial code

---
+++ src/test/java/medicare/Constant.java
@@ -0,0 +1,8 @@
+package medicare;
+
+public class Constant {
+
+	public static final String DRIVER_PATH = "/home/utkarshagrawal/Downloads/geckodriver-v0.29.1-linux64/geckodriver";
+	
+	public static final String BASE_PATH = "http://localhost:8080/Medicare/";
+}
