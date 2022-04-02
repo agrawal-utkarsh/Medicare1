69b81520fb3f5a2c2825a325e3abf03fd31bf0c5
agrawal-utkarsh <utkarsh.agarwal@mobisy.com>
2022-04-02 11:35:47 +0530

    corrected path

--- src/test/java/medicare/Constant.java
+++ src/test/java/medicare/Constant.java
@@ -2,7 +2,7 @@ package medicare;
 
 public class Constant {
 
-	public static final String DRIVER_PATH = "/home/utkarshagrawal/Downloads/geckodriver-v0.29.1-linux64/geckodriver";
-	
-	public static final String BASE_PATH = "http://localhost:8080/Medicare/";
+	public static final String DRIVER_PATH = "/home/ubuntu/geckodriver-v0.29.1-linux64/geckodriver";
+
+	public static final String BASE_PATH = "http://localhost:8081/Medicare/";
 }
